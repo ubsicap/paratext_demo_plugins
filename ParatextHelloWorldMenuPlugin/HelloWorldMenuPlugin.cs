f9d5ea8dfce4e275097c4549942804229cc175d8
FoolRunning <foolrunning@gmail.com>
2021-02-15 13:01:35 -0500

    Start of work on new API

--- ParatextHelloWorldMenuPlugin/HelloWorldMenuPlugin.cs
+++ ParatextHelloWorldMenuPlugin/HelloWorldMenuPlugin.cs
@@ -1,26 +1,36 @@
 ﻿using System;
-using System.AddIn;
-using System.AddIn.Pipeline;
 using System.Collections.Generic;
 using System.Windows.Forms;
-using AddInSideViews;
+using Paratext.PluginInterfaces;
 
 namespace ParatextHelloWorldMenuPlugin
 {
-    /// <summary>
-    /// Simple plugin that shows the active project when it's menu item is clicked.
-    /// </summary>
-    [AddIn("Hello World", Description = "Displays a message box when a menu item is clicked.", Version = "1.0", Publisher = "SIL/UBS")]
-    [QualificationData(PluginMetaDataKeys.menuText, "Hello World Demo Menu Plugin...")]
-    [QualificationData(PluginMetaDataKeys.insertAfterMenuName, "Tools|Advanced|Simplify Paratext Menus")]
-    [QualificationData(PluginMetaDataKeys.multipleInstances, CreateInstanceRule.always)]
-    public class HelloWorldMenuPlugin : IParatextAddIn
+    public class HelloWorldMenuPlugin : IParatextStandalonePlugin
     {
+        public string Name => "Hello World";
+        
+        public string Description => "Displays a message box when a menu item is clicked.";
+        
+        public Version Version => new Version(2, 0);
+        
+        public string VersionString => Version.ToString();
+        
+        public string Publisher => "SIL/UBS";
+        
+        public IEnumerable<IPluginMenuEntry<IPluginHost>> PluginMenuEntries
+        {
+            get
+            {
+                yield return new MenuEntry();
+            }
+        }
+
         /// <summary>
         /// Called by Paratext when the menu item created for this plugin was clicked.
         /// </summary>
-        public void Run(IHost host, string activeProjectName)
+        private static void Run(IPluginHost host, IParatextChildWindow window)
         {
+            host.ActiveWindow
             string message = string.IsNullOrEmpty(activeProjectName) ?
                 "You clicked the menu item when there was no active project." :
                 string.Format("You clicked the menu item while the {0} project was active.", activeProjectName);
@@ -29,14 +39,24 @@ namespace ParatextHelloWorldMenuPlugin
             Environment.Exit(0);
         }
 
-        public void RequestShutdown()
+        private class MenuEntry : IPluginMenuEntry<IPluginHost>
         {
-            // Just return and let Paratext kill the plugin process if the message box is still showing.
-        }
+            public string GetText(string locale)
+            {
+                switch (locale)
+                {
+                    case "es": return "Hola mundo...";
+                    default: return "Hello World Demo Menu Plugin...";
+                }
+            }
 
-        public Dictionary<string, IPluginDataFileMergeInfo> DataFileKeySpecifications
-        {
-            get { return null; }
+            public PluginMenuLocation Location => PluginMenuLocation.ParatextAdvanced;
+
+            public IEnumerable<string> InsertAfterMenuHierarchy => null;
+
+            public string ImagePath => null;
+            public WhenToShow ShowWhen => WhenToShow.Always;
+            public Action<IPluginHost, IParatextChildWindow> Clicked => Run;
         }
     }
 }
