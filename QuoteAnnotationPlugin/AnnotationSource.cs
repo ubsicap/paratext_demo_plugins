aeed42863e03df32e588c11acb92a8fdaaa76a1b
FoolRunning <foolrunning@gmail.com>
2021-05-11 14:10:24 -0400

    Added new demo plugin to demonstrate annotating Scripture text

---
+++ QuoteAnnotationPlugin/AnnotationSource.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Text.RegularExpressions;
+using Paratext.PluginInterfaces;
+
+namespace QuoteAnnotationPlugin
+{
+    internal class AnnotationSource : IPluginAnnotationSource
+    {
+        private readonly IQuotationMarkInfo quotationMarks;
+        private readonly List<Regex> findMarksRegexes = new List<Regex>();
+        
+        public AnnotationSource(IProject project)
+        {
+            quotationMarks = project.Language.QuotationMarkInfo;
+            HashSet<string> allMarks = new HashSet<string>();
+
+            // NOTE: This doesn't actually work correctly if multiple levels contain the same quotation marks,
+            // but figuring out the nesting is a lot more work then we want to attempt for this demo plugin.
+            
+            for (int level = 0; level < quotationMarks.PrimaryLevels.Count; level++)
+            {
+                IQuotationMarkLevel lev = quotationMarks.PrimaryLevels[level];
+
+                StringBuilder bldr = new StringBuilder();
+
+                if (!allMarks.Contains(lev.Opener))
+                {
+                    allMarks.Add(lev.Opener);
+                    bldr.Append(Regex.Escape(lev.Opener));
+                }
+
+                if (!allMarks.Contains(lev.Closer))
+                {
+                    allMarks.Add(lev.Closer);
+                    if (bldr.Length > 0)
+                        bldr.Append('|');
+                    bldr.Append(Regex.Escape(lev.Closer));
+                }
+
+                if (!allMarks.Contains(lev.Continuer))
+                {
+                    allMarks.Add(lev.Continuer);
+                    if (bldr.Length > 0)
+                        bldr.Append('|');
+                    bldr.Append(Regex.Escape(lev.Continuer));
+                }
+
+                findMarksRegexes.Add(bldr.Length > 0 ? new Regex(bldr.ToString(), RegexOptions.Compiled) : null);
+            }
+        }
+
+        #region Implementation of IPluginAnnotationSource
+        public event EventHandler AnnotationsChanged;
+
+        public bool MaintainSelectionsOnWordBoundaries => false;
+
+        public IReadOnlyList<AnnotationStyle> GetStyleInfo(double zoom)
+        {
+            return new[]
+            {
+                new AnnotationStyle("quote0", "background-color:MediumOrchid;"),
+                new AnnotationStyle("quote1", "background-color:Cyan;"),
+                new AnnotationStyle("quote2", "background-color:Chartreuse;"),
+                new AnnotationStyle("quote3", "background-color:RosyBrown;")
+            };
+        }
+
+        public IReadOnlyList<IPluginAnnotation> GetAnnotations(IVerseRef verseRef, string usfm)
+        {
+            List<IPluginAnnotation> annotations = new List<IPluginAnnotation>();
+            for (int level = 0; level < findMarksRegexes.Count; level++)
+            {
+                if (findMarksRegexes[level] == null)
+                    continue;
+                foreach (Match match in findMarksRegexes[level].Matches(usfm))
+                {
+                    Selection sel = new Selection(match.Value, usfm.Substring(0, match.Index),
+                        usfm.Substring(match.Index + match.Length), verseRef, match.Index);
+                    annotations.Add(new Annotation(sel, "quote" + level));
+                }
+            }
+
+            return annotations;
+        }
+        #endregion
+
+        #region Annotation class
+        private sealed class Annotation : IPluginAnnotation
+        {
+            public Annotation(IScriptureTextSelection selection, string styleName)
+            {
+                ScriptureSelection = selection;
+                StyleName = styleName;
+            }
+            public IScriptureTextSelection ScriptureSelection { get; }
+
+            public string StyleName { get; }
+
+            public string Icon => null;
+
+            public string IconToolTipText => null;
+            
+            public bool Click(MouseButton button, bool onIcon, Point location)
+            {
+                return false;
+            }
+        }
+        #endregion
+        
+        #region Selection class
+        private sealed class Selection : IScriptureTextSelection
+        {
+            public Selection(string selectedText, string beforeContext, string afterContext,
+                IVerseRef verseRef, int offset)
+            {
+                SelectedText = selectedText;
+                BeforeContext = beforeContext;
+                AfterContext = afterContext;
+                VerseRefStart = verseRef;
+                VerseRefEnd = verseRef;
+                Offset = offset;
+            }
+
+            public IVerseRef VerseRefStart { get; }
+
+            public IVerseRef VerseRefEnd { get; }
+
+            public string SelectedText { get; }
+
+            public int Offset { get; }
+
+            public string BeforeContext { get; }
+
+            public string AfterContext { get; }
+
+            public bool Equals(ISelection other)
+            {
+                throw new NotImplementedException();
+            }
+        }
+        #endregion
+    }
+}
---
+++ QuoteAnnotationPlugin/QuotationAnnotationPlugin.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.XPath;
+using Paratext.PluginInterfaces;
+
+namespace QuoteAnnotationPlugin
+{
+    /// <summary>
+    /// Simple plugin that shows a text box that the user can enter text into. The text is
+    /// then persisted with the other Paratext project data.
+    /// </summary>
+    public class QuotationAnnotationPlugin : IParatextScrTextAnnotationPlugin
+    {
+        public string Name => "Quote Marking Plugin";
+
+        public string Description => "Highlights the quotation marks of a project in the project text.";
+
+        public Version Version => new Version(1, 0);
+
+        public string VersionString => Version.ToString();
+
+        public string Publisher => "SIL/UBS";
+
+        public IEnumerable<KeyValuePair<string, XMLDataMergeInfo>> MergeDataInfo => null;
+        
+        public IEnumerable<PluginAnnotationMenuEntry> PluginAnnotationMenuEntries 
+        {
+            get 
+            { 
+                PluginAnnotationMenuEntry entry = new PluginAnnotationMenuEntry("Highlight quotation marks", 
+                    proj => new AnnotationSource(proj));
+
+                entry.LocalizedTextNeeded += delegate(string defaultText, string locale)
+                {
+                    switch (locale)
+                    {
+                        case "es": return "Resaltar las comillas";
+                        case "de": return "Anführungszeichen markieren";
+                        default: return defaultText;
+                    }
+                };
+                yield return entry;
+            }
+        }
+    }
+}
