e21bf913457ac086dcc0654ea5f348e145131500
Mike Pelley <61086313+mpelley@users.noreply.github.com>
2021-08-13 20:02:12 -0400

    Add ReferencePluginN for displaying styles

---
+++ ReferencePluginN/ControlN.Designer.cs
@@ -0,0 +1,64 @@
+﻿
+namespace ReferencePluginN
+{
+	partial class ControlN
+	{
+		/// <summary> 
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Component Designer generated code
+
+		/// <summary> 
+		/// Required method for Designer support - do not modify 
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBox = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// textBox
+			// 
+			this.textBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+				| System.Windows.Forms.AnchorStyles.Left) 
+				| System.Windows.Forms.AnchorStyles.Right)));
+			this.textBox.Location = new System.Drawing.Point(0, 0);
+			this.textBox.Multiline = true;
+			this.textBox.Name = "textBox";
+			this.textBox.ReadOnly = true;
+			this.textBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+			this.textBox.Size = new System.Drawing.Size(955, 447);
+			this.textBox.TabIndex = 0;
+			// 
+			// ControlN
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.textBox);
+			this.Name = "ControlN";
+			this.Size = new System.Drawing.Size(955, 447);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBox;
+	}
+}
---
+++ ReferencePluginN/PluginN.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Paratext.PluginInterfaces;
+
+
+namespace ReferencePluginN
+{
+    public class PluginN : IParatextWindowPlugin
+    {
+		public const string pluginName = "Reference Plugin N";
+		public string Name => pluginName;
+		public string GetDescription(string locale) => "Demonstrates accessing style info.";
+		public Version Version => new Version(1, 0);
+		public string VersionString => Version.ToString();
+		public string Publisher => "SIL/UBS";
+		public IEnumerable<KeyValuePair<string, XMLDataMergeInfo>> MergeDataInfo => null;
+
+		public IEnumerable<WindowPluginMenuEntry> PluginMenuEntries
+		{
+			get
+			{
+				yield return new WindowPluginMenuEntry("PluginN...", Run, PluginMenuLocation.ScrTextProject);
+			}
+		}
+
+		public void Run(IWindowPluginHost host, IParatextChildState windowState)
+		{
+			ControlN theControl = new ControlN();
+			host.ShowEmbeddedUi(theControl, windowState.Project);
+		}
+	}
+}
