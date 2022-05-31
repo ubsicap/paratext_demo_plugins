d99d8a3a9cd20d62d8f95819c8e4b9ef9d66d532
Mike Pelley <michael_pelley@sil.org>
2022-05-31 17:23:43 -0400

    Add Plugin Q for story 22435

---
+++ ReferencePluginQ/PluginQ.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Paratext.PluginInterfaces;
+
+namespace ReferencePluginQ
+{
+    public class PluginQ : IParatextWindowPlugin
+    {
+        public const string pluginName = "Reference Plugin Q";
+        public string Name => pluginName;
+        public string GetDescription(string locale) => "Demonstrates using Text Collections.";
+        public Version Version => new Version(1, 0);
+        public string VersionString => Version.ToString();
+        public string Publisher => "SIL/UBS";
+
+        public IEnumerable<WindowPluginMenuEntry> PluginMenuEntries
+        {
+            get
+            {
+                yield return new WindowPluginMenuEntry("PluginQ...", Run, PluginMenuLocation.ParatextAdvanced);
+            }
+        }
+
+        public IDataFileMerger GetMerger(IPluginHost host, string dataIdentifier) => throw new NotImplementedException();
+
+        public void Run(IWindowPluginHost host, IParatextChildState windowState)
+        {
+            ControlQ theControl = new ControlQ();
+            host.ShowEmbeddedUi(theControl, null);
+        }
+    }
+}
